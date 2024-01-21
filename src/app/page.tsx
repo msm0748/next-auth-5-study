a105d0345a485e92094c500fab631c3d7382772e
seokmin <msm0748@gmail.com>
2024-01-22 02:20:30 +0900

    feat: 로그인, 마이페이지 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <main>Home</main>;
+import Link from 'next/link';
+import { auth } from '../auth';
+
+export default async function Home() {
+  // const session = await auth();
+  // console.log(session, '홈');
+  return (
+    <div>
+      <main>Home page</main>
+      <ul>
+        <li className=" text-blue-600">
+          <Link href="/login">로그인</Link>
+        </li>
+        <li className=" text-blue-600">
+          <Link href="mypage">마이페이지</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
